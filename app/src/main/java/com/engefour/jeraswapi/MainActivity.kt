c165f33d310c8b701b0b8633848dce865dfd5259
MarceloAMDS <Marcelo1701!>
2019-04-28 18:06:18 -0400

    Custom adapter do Groupie adicionado

--- app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
+++ app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
@@ -1,36 +1,64 @@
 package com.engefour.jeraswapi
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.engefour.jeraswapi.model.api.StarWarsApi
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_movies.view.*
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import androidx.recyclerview.widget.GridLayoutManager
+
+
 
 class MainActivity : AppCompatActivity() {
-    var listView : ListView? = null
-    var movies = mutableListOf<String>()
-    var movieAdapter : ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        listView = ListView(this)
-        setContentView(listView)
-        movieAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,movies)
-        listView?.adapter = movieAdapter
+        setContentView(R.layout.activity_main)
+
+        val movieAdapter = GroupAdapter<ViewHolder>()
+        listMovies.adapter = movieAdapter
+        listMovies.layoutManager = GridLayoutManager(this, 2)
 
         val api = StarWarsApi()
         api.loadMovies()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe( {
-                movie -> movies.add(movie.title)
+                movie -> movieAdapter.add(MovieItem(movie.title,movie.episodeId))
             },{
                 e -> e.printStackTrace()
             },{
-                movieAdapter?.notifyDataSetChanged()
+                movieAdapter.notifyDataSetChanged()
             })
 
     }
+
+    //Classe do Groupie que infla a view para o listview
+    class MovieItem(private val movieTitle:String, private val episodeId:Int): Item<ViewHolder>(){
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            viewHolder.itemView.textViewTitle.text = movieTitle
+            viewHolder.itemView.setOnClickListener {
+//                val i = Intent(context,MovieActivity::class.java)
+//                i.putExtra("userId", userId)
+//                context?.startActivity(i)
+            }
+        }
+        override fun getLayout(): Int {
+            return R.layout.row_movies
+        }
+
+        override fun getSpanSize(spanCount: Int, position: Int) = spanCount/2
+    }
 }
+
+
