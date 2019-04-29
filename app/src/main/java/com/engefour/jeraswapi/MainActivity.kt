b5dda410b98956c80481f3b2dc1113d43e128a86
MarceloAMDS <marcelo.augusto9@hotmail.com>
2019-04-29 19:15:56 -0400

    Inicio de remodelagem tela inicial. Fotos dos filmes adicionadas

--- app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
+++ app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
@@ -12,9 +12,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.row_movies.view.*
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.engefour.jeraswapi.model.Filme
 import com.google.gson.Gson
+import android.os.Build
+import android.graphics.drawable.Drawable
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,7 +31,15 @@ class MainActivity : AppCompatActivity() {
 
         val movieAdapter = GroupAdapter<ViewHolder>()
         listMovies.adapter = movieAdapter
-        listMovies.layoutManager = GridLayoutManager(this, 2)
+        listMovies.layoutManager = LinearLayoutManager(this)
+
+        Glide.with(this).load(R.drawable.background).centerCrop()
+            .into(object : SimpleTarget<Drawable>() {
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                mainLayout.setBackground(resource)
+            }
+        })
+
 
         val api = StarWarsApi()
         api.loadMovies()
@@ -40,6 +54,11 @@ class MainActivity : AppCompatActivity() {
             },{
                 //onComplete - quando completa todas as requisições
                 movieAdapter.notifyDataSetChanged()
+                Glide.with(this).load(R.drawable.background).into(object : SimpleTarget<Drawable>() {
+                    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                        mainLayout.setBackground(resource)
+                    }
+                })
             })
 
     }
@@ -53,12 +72,20 @@ class MainActivity : AppCompatActivity() {
                 i.putExtra("movie", Gson().toJson(movie))
                 context.startActivity(i)
             }
+
+            when(movie.episodeId){
+                1-> Glide.with(viewHolder.itemView).load(R.drawable.thephantonmenace).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                2 -> Glide.with(viewHolder.itemView).load(R.drawable.attackoftheclones).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                3 -> Glide.with(viewHolder.itemView).load(R.drawable.revengeofthesith).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                4 -> Glide.with(viewHolder.itemView).load(R.drawable.anewhope).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                5 -> Glide.with(viewHolder.itemView).load(R.drawable.returnofthejedi).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                6 -> Glide.with(viewHolder.itemView).load(R.drawable.theempirestrikesback).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                7 -> Glide.with(viewHolder.itemView).load(R.drawable.theforceawakens).centerCrop().into(viewHolder.itemView.imageViewPoster)
+            }
         }
         override fun getLayout(): Int {
             return R.layout.row_movies
         }
-
-        override fun getSpanSize(spanCount: Int, position: Int) = spanCount/2
     }
 }
 
