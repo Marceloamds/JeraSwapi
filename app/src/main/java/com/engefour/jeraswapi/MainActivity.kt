5caa3be5e5d7d0870a7cdb6cd2f2acc7ea1df97d
MarceloAMDS <Marcelo1701!>
2019-04-30 02:07:24 -0400

    Adicionadas informações completas a MainActivity Adicionadas fotos novas aos filmes

--- app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
+++ app/src/main/java/com/engefour/jeraswapi/MainActivity.kt
@@ -16,9 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.engefour.jeraswapi.model.Filme
 import com.google.gson.Gson
-import android.os.Build
 import android.graphics.drawable.Drawable
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
 
@@ -32,14 +30,16 @@ class MainActivity : AppCompatActivity() {
         val movieAdapter = GroupAdapter<ViewHolder>()
         listMovies.adapter = movieAdapter
         listMovies.layoutManager = LinearLayoutManager(this)
+        listMovies.isNestedScrollingEnabled = false
+        listMovies.isFocusable = false
 
         Glide.with(this).load(R.drawable.background).centerCrop()
             .into(object : SimpleTarget<Drawable>() {
             override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
-                mainLayout.setBackground(resource)
+                mainLayout.background = resource
             }
         })
-
+        Glide.with(this).load(R.drawable.logo).into(imageViewLogo)
 
         val api = StarWarsApi()
         api.loadMovies()
@@ -67,6 +67,9 @@ class MainActivity : AppCompatActivity() {
     class MovieItem(private val context: Context,private val movie: Filme): Item<ViewHolder>(){
         override fun bind(viewHolder: ViewHolder, position: Int) {
             viewHolder.itemView.textViewTitle.text = movie.title
+            val yearRelease = 1900+ movie.releaseDate.year
+            viewHolder.itemView.textViewDate.text = "Released in $yearRelease"
+            viewHolder.itemView.textViewDirector.text = "Directed by "+movie.director
             viewHolder.itemView.setOnClickListener {
                 val i = Intent(context,MovieDetailActivity::class.java)
                 i.putExtra("movie", Gson().toJson(movie))
@@ -74,13 +77,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             when(movie.episodeId){
-                1-> Glide.with(viewHolder.itemView).load(R.drawable.thephantonmenace).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                1-> Glide.with(viewHolder.itemView).load(R.drawable.thephantommenace).centerCrop().into(viewHolder.itemView.imageViewPoster)
                 2 -> Glide.with(viewHolder.itemView).load(R.drawable.attackoftheclones).centerCrop().into(viewHolder.itemView.imageViewPoster)
                 3 -> Glide.with(viewHolder.itemView).load(R.drawable.revengeofthesith).centerCrop().into(viewHolder.itemView.imageViewPoster)
                 4 -> Glide.with(viewHolder.itemView).load(R.drawable.anewhope).centerCrop().into(viewHolder.itemView.imageViewPoster)
-                5 -> Glide.with(viewHolder.itemView).load(R.drawable.returnofthejedi).centerCrop().into(viewHolder.itemView.imageViewPoster)
-                6 -> Glide.with(viewHolder.itemView).load(R.drawable.theempirestrikesback).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                5 -> Glide.with(viewHolder.itemView).load(R.drawable.empirestrikesback).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                6 -> Glide.with(viewHolder.itemView).load(R.drawable.returnofthejedi).centerCrop().into(viewHolder.itemView.imageViewPoster)
                 7 -> Glide.with(viewHolder.itemView).load(R.drawable.theforceawakens).centerCrop().into(viewHolder.itemView.imageViewPoster)
+                else -> Glide.with(viewHolder.itemView).load(R.drawable.placeholder).centerCrop().into(viewHolder.itemView.imageViewPoster)
             }
         }
         override fun getLayout(): Int {
